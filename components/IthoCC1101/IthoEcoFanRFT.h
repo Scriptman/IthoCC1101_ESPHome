a6bf7b31680ac351c78593e75aa704f8a653a36c
Mario Lamers <mlamers@assert-development.nl>
2025-02-13 15:34:03 +0100

    Moved __INIT__.py

---
+++ components/IthoCC1101/IthoEcoFanRFT.h
@@ -0,0 +1,34 @@
+#ifndef ITHOECOFANRFT_H
+#define ITHOECOFANRFT_H
+
+#include "esphome.h"
+#include "esphome/components/fan/fan.h"
+#include <IthoCC1101.h>
+
+class IthoCC1101Component : public Component, public fan::Fan {
+public:
+    void setup() override;
+    void loop() override;
+
+    void set_irq_pin(GPIOPin *pin) { _irq_pin = pin; }
+    //void set_fan_speed(float speed);
+
+protected:
+    GPIOPin *_irq_pin;
+    bool ITHOhasPacket = false;
+
+    void control(const fan::FanCall &call) override;
+    void write_state_();
+
+private:
+    IthoCC1101 rf;
+    float last_speed = -1;
+
+    #if defined (ESP82666) || defined (ESP32)
+    IRAM_ATTR void ITHOCheck();
+    #else
+    void ITHOCheck();
+    #endif
+};
+
+#endif
